08a3d4f5a8a9ec016a8040cc4c2cc3d5b5877821
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-14 20:47:19 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -11,24 +11,24 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, location, current_player = "X")
-  board[location] = current_player
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
 end
 
-def valid_move?(board, location)
-  index.between?(0,8) && !position_taken?(board, location)
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  location = input.to_i - 1
-  if valid_move?(board, location)
-    move(board, location)
+  index = input.to_i - 1
+  if valid_move?(board, index)
+    move(board, index)
     display_board(board)
   else
     turn(board)
